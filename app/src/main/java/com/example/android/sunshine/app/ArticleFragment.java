d62ae5718668a2b0543b6d7cf7c4ec8f26759e1e
chkumar <himakiran@gmail.com>
2016-11-06 11:42:44 +0530

    Sunshine first commit
    Created a new project Sunshine with a separate fragment class and then
    added the fragment into the activity_main.xml. the fragment shares the
    same layout file as the main activity class. To get the app running had
    to comment out calling the layout from the main activity.

---
+++ app/src/main/java/com/example/android/sunshine/app/ArticleFragment.java
@@ -0,0 +1,21 @@
+package com.example.android.sunshine.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by userhk on 06/11/16.
+ */
+
+
+public class ArticleFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+}
