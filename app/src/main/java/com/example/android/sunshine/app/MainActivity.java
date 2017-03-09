ede1acfcd25a2957c358f5e83d4711465557d4a5
himakiran <himakiran@gmail.com>
2017-03-09 14:08:26 +0530

    Implemented actionbar themes.. but due to min api level 15 cannot color the statusbar

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             }
         } else {
             mTwoPane = false;
+            getSupportActionBar().setElevation(0f);
         }
 
         ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager()
