bb2e91286cd4b142d92c51b63441627b673c32c4
himakiran <himakiran@gmail.com>
2017-04-02 21:58:22 +0530

    Revert "changed code in settings.java"
    This reverts commit 43edd903d1d1e4fcc338132bf00b3954328cd7c2.

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -2,16 +2,12 @@ package com.example.android.sunshine.app;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-
-import com.example.android.sunshine.app.data.WeatherContract;
-import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
 
 import static com.example.android.sunshine.app.R.xml.preferences;
 
@@ -24,7 +20,7 @@ import static com.example.android.sunshine.app.R.xml.preferences;
  * API Guide</a> for more information on developing a Settings UI.
  */
 public class SettingsActivity extends PreferenceActivity
-        implements Preference.OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,21 +45,6 @@ public class SettingsActivity extends PreferenceActivity
 
     }
 
-    // Registers a shared preference change listener that gets notified when preferences change
-    @Override
-    protected void onResume() {
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        sp.registerOnSharedPreferenceChangeListener(this);
-        super.onResume();
-    }
-
-    // Unregisters a shared preference change listener
-    @Override
-    protected void onPause() {
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        sp.unregisterOnSharedPreferenceChangeListener(this);
-        super.onPause();
-    }
     /**
      * Attaches a listener so the summary is always updated with the preference value.
      * Also fires the listener once, to initialize the summary (so it shows up before the value
@@ -73,16 +54,17 @@ public class SettingsActivity extends PreferenceActivity
         // Set the listener to watch for value changes.
         preference.setOnPreferenceChangeListener(this);
 
-        // Set the preference summaries
-        setPreferenceSummary(preference,
+        // Trigger the listener immediately with the preference's
+        // current value.
+        onPreferenceChange(preference,
                 PreferenceManager
                         .getDefaultSharedPreferences(preference.getContext())
                         .getString(preference.getKey(), ""));
     }
 
-    private void setPreferenceSummary(Preference preference, Object value) {
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
         String stringValue = value.toString();
-        String key = preference.getKey();
 
         if (preference instanceof ListPreference) {
             // For list preferences, look up the correct display value in
@@ -96,25 +78,9 @@ public class SettingsActivity extends PreferenceActivity
             // For other preferences, set the summary to the value's simple string representation.
             preference.setSummary(stringValue);
         }
-    }
-    // This gets called before the preference is changed
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object value) {
-        setPreferenceSummary(preference, value);
         return true;
     }
 
-    // This gets called after the preference is changed, which is important because we
-    // start our synchronization here
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if ( key.equals(getString(R.string.pref_location_key)) ) {
-            SunshineSyncAdapter.syncImmediately(this);
-        } else if ( key.equals(getString(R.string.pref_units_key)) ) {
-            // units have changed. update lists of weather entries accordingly
-            getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
-        }
-    }
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public Intent getParentActivityIntent() {
